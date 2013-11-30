2b147c79aca5888ac37dbccbad59aed951813327
maass-sql <maassql@gmail.com>
2013-11-30 15:20:57 -0600

    get a little closer to testing for a viable repository

--- common/scm/create_repository.ps1
+++ common/scm/create_repository.ps1
@@ -3,12 +3,18 @@ Function create_repository ($repository_name, $repository_path)
     # https://help.github.com/articles/create-a-repo
 
     $full_path = "$repository_path\$repository_name"
+    write-host "create_repository | BEGIN | $full_path"
+
 
     if ((Test-Path $full_path -IsValid) -eq $false)                 { throw "The full repository path is invalid.  Full repo path=[$full_path]."}
     if ((Test-Path -LiteralPath:$repository_path) -eq $false )      { throw "The repository_path must exist before calling this function.  repository_path=[$repository_path]."}
-    if ((Test-Path -LiteralPath:$full_path) -eq $false )    
+    if ((Test-Path -LiteralPath:"$full_path\.git") -eq $false )    
         { 
-            md $full_path 
+            if ((Test-Path -LiteralPath:$full_path) -eq $false )  
+            {
+                md $full_path
+            }
+            
             cd $full_path
 
             $git_args = @('init', ">> $(log_file_name)")
@@ -25,5 +31,6 @@ Function create_repository ($repository_name, $repository_path)
             write-host "$commit_results"              
         }
 
+    write-host "create_repository | END | $full_path"
     return $full_path 
 }
