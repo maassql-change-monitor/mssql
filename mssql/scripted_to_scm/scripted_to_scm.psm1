6127f60f2604e86ec45cbdbe07035acf7ad9ac83
maass-sql <maassql@gmail.com>
2013-11-30 16:02:06 -0600

    wip

--- mssql/scripted_to_scm/scripted_to_scm.psm1
+++ mssql/scripted_to_scm/scripted_to_scm.psm1
@@ -17,7 +17,7 @@ function scripted_to_scm ()
         cd $SCRIPT:scripted_db_directory_base_path  <# Just cause there is all of this cd ng around in the rest of the code, reset at the top of this loop #>
         $scrptd = (scripted_db_properties $scripted_db_directory)
         $commit_msg = "InstanceName=[$($scrptd.'instance')].  Db=[$($scrptd.'dbname')].  scripted_to_scm automation. Captured on=[$($scrptd.'dttm')]."
-        scripted_to_scm_log "Calling snapshot_commit -remove_snapshot_path -clear_repository_after_commit -local_repository_path:($scrptd.'scm_db_path') -local_snapshot_path:($scrptd.'path') -snapshot_commit_message:$commit_msg "
+        scripted_to_scm_log "Calling snapshot_commit -remove_snapshot_path -clear_repository_after_commit -local_repository_path:$($scrptd.'scm_db_path') -local_snapshot_path:$($scrptd.'path') -snapshot_commit_message:$commit_msg "
         $null = ( snapshot_commit -remove_snapshot_path -clear_repository_after_commit -local_repository_path:($scrptd.'scm_db_path') -local_snapshot_path:($scrptd.'path') -snapshot_commit_message:$commit_msg )
         scripted_to_scm_log "scripted_to_scm- bottom of loop.  scripted_db_directory=[$scripted_db_directory]."
     }
