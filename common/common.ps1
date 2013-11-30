ad1355dc82ee227b0c61afc8e83f03fa5e646a5b
maass-sql <maassql@gmail.com>
2013-11-30 12:19:18 -0600

    apparently dot sourcing psm1 doesn't work like I thought it did

--- common/common.ps1
+++ common/common.ps1
@@ -1,4 +1,4 @@
 $here =  ( Split-Path $MyInvocation.MyCommand.Definition )
 . "$here\loop_d_loop.ps1"
-. "$here\file_system_nuke\file_system_nuke.psm1"
+Import-Module -Force "$here\file_system_nuke\file_system_nuke.psm1" <# did you know that?  I did NOT know that!  ."path.psm1" --> actually opens the file.  In my case, in notepad #>
 . "$here\scm\scm.ps1"
