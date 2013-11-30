3d5b8655fd6f663aa6ef13885f47bbab15ecb52e
maass-sql <maassql@gmail.com>
2013-11-30 13:36:10 -0600

    let's try it this way

--- mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
+++ mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
@@ -2,15 +2,15 @@ Function scripted_db_directories_to_copy ()
 {
     scripted_to_scm_log "scripted_db_directories_to_copy- BEGIN"
     $ret_array = @()
-    foreach ( $scripted_dir in Get-ChildItem -LiteralPath:$SCRIPT:scripted_db_directory_base_path 
-        | Where-Object { $_.PSIsContainer -eq $true } 
-        | Where-Object 
+    foreach ( $scripted_dir in Get-ChildItem -LiteralPath:$SCRIPT:scripted_db_directory_base_path |
+        Where-Object { $_.PSIsContainer -eq $true } |
+        Where-Object 
             { 
                 $tspan=(New-TimeSpan $scripted_dir.LastAccessTime (Get-Date));
                 $diff_minutes=($tspan).minutes;
                 return ( $diff_minutes -ge $SCRIPT:scripted_db_directory_must_sit_idle_for_x_minutes ) 
-            }  
-        | Select-Object $_ -First:$SCRIPT:directories_to_grab_at_a_time ) <# -Directory is not in 2.0 #>
+            }  |
+        Select-Object $_ -First:$SCRIPT:directories_to_grab_at_a_time ) <# -Directory is not in 2.0 #>
     {
         $ret_array += $scripted_dir 
     }
