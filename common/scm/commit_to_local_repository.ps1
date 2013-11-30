de9d46454e09a297ccc3061312de33e21b16047f
maass-sql <maassql@gmail.com>
2013-11-30 14:31:01 -0600

    this probably is not going to work

--- common/scm/commit_to_local_repository.ps1
+++ common/scm/commit_to_local_repository.ps1
@@ -5,7 +5,7 @@ Function commit_to_local_repository ($path_to_commit, $msg)
     cd $path_to_commit
 
     write-host "commit_to_local_repository - Adding files" <# stage updates/deletes for ALL files, including new ones # Also a leading directory name (e.g. dir to add dir/file1 and dir/file2) can be given to add all files in the directory, recursively. #>
-    $git_args = @('add', "--all" , "$path_to_commit", ">>$(log_file_name)")
+    $git_args = @('add', "--all" , "$path_to_commit", ">> $(log_file_name)")
     $add_results = (& $git_path $git_args )
     write-host "commit_to_local_repository of=[$path_to_commit] - added=[$add_results]"
 
@@ -13,7 +13,7 @@ Function commit_to_local_repository ($path_to_commit, $msg)
     write-host "commit_to_local_repository - Committing files"<# stage updates/deletes for files git already knows about AND COMMIT #>
 
     $msg_arg = "--message='$($msg)'"
-    $git_args = @('commit', $msg_arg, ">>$(log_file_name)")
+    $git_args = @('commit', $msg_arg, ">> $(log_file_name)")
     $commit_results = (& $git_path $git_args )
     write-host "commit_to_local_repository - commit=[$commit_results]"
     <# TODO : look at $commit_results for [master ff61ceb] or for ? #>
