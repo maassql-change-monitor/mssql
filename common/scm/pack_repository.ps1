9d5ab47a3406c5b5d280cdce29468875e1b2a7de
maass-sql <maassql@gmail.com>
2013-11-30 15:20:21 -0600

    correct logging

--- common/scm/pack_repository.ps1
+++ common/scm/pack_repository.ps1
@@ -1,12 +1,12 @@
 Function pack_repository ($path_to_repository)
 {
-    write-host "git_maintenance BEGIN"
+    write-host "pack_repository BEGIN `$path_to_repository=[$path_to_repository]"
 
     cd $path_to_repository
 
     $git_args = @('gc', '--aggressive', ">> $(log_file_name)")
     $maint_results = (& $SCRIPT:git_path $git_args )
-    write-host "git_maintenance - maint_results=[$maint_results]"
+    write-host "pack_repository - maint_results=[$maint_results]"
 
-    write-host "git_maintenance END"  
+    write-host "pack_repository END"  
 }
