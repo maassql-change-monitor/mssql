90bc75c283bdafe5cbdf4afe25acc05546aede13
maass-sql <maassql@gmail.com>
2013-11-30 15:30:45 -0600

    wip

--- common/file_system_nuke/nuke_directory.ps1
+++ common/file_system_nuke/nuke_directory.ps1
@@ -7,17 +7,18 @@ function nuke_directory
 
     [cmdletbinding()]
     Param (
-        [Parameter(Mandatory=$true)][string]   $dir_to_nuke
+        [Parameter(Mandatory=$true)][string]        $dir_to_nuke
         ,[Parameter(Mandatory=$false)][object[]]    $names_to_leave
-        ,[Parameter(Mandatory=$false)] [switch]    $leave_directory
+        ,[Parameter(Mandatory=$false)] [switch]     $leave_directory
     )
 
-
-
-
-
-
     if ( $names_to_leave -eq $null ) { $names_to_leave = @() }
+
+    foreach($n in $names_to_leave)
+    {
+      write-host "name to leave=$n"
+    }
+
 
     $SCRIPT:files_in_use = $null
     $SCRIPT:files_in_use = New-Object System.Collections.ArrayList
@@ -63,23 +64,26 @@ function nuke_directory
         write-debug "     `$delete_one_at_time =[$delete_one_at_time]"
         if ( $delete_one_at_time -eq $true )
         {
+
+          $null = (nuke_directory_one_file_at_a_time -dir_to_nuke:$dir_to_nuke  -names_to_leave:$names_to_leave)
+
           if ( $leave_directory -eq $false )
-          {
-            $null = (nuke_directory_one_file_at_a_time -dir_to_nuke:$dir_to_nuke  -names_to_leave:$names_to_leave)
-            write-debug "     All Files are now probably gone.  Trying to delete the entire directory again."
-            if ($dir_to_remove_info.Exists -eq $true)
-              {
-                try 
+            {
+              write-debug "     All Files are now probably gone.  Trying to delete the entire directory again."
+              if ($dir_to_remove_info.Exists -eq $true)
                 {
-                  $dir_to_remove_info.Delete($true) 
+                  try 
+                  {
+                    $dir_to_remove_info.Delete($true) 
+                  }
+                  catch {[Exception]}
+                  {
+                    write-host "Exception occurred trying to nuke directory after we thought it was empty.  Open fileHandles to print below this message.  Exception=$($_.Exception.Message)"
+                    GET-Openfile -Filename:$dir_to_remove_info.FullName | Format-Table | Write-Host
+                    throw $_.Exception.Message
+                  }
                 }
-                catch {[Exception]}
-                {
-                  GET-Openfile -Filename:$dir_to_remove_info.FullName | Format-Table | Write-Host
-                  throw $_.Exception.Message
-                }
-              }
-          }          
+            }          
         }
     }
     else 
--- common/scm/clear_repository.ps1
+++ common/scm/clear_repository.ps1
@@ -1,6 +1,6 @@
 function clear_repository($dir_to_remove)
 {
     $names_to_leave = @('.git', '.gitignore')
-    $null = ( nuke_directory -dir_to_nuke:$dir_to_remove $names_to_leave -leave_directory )
+    $null = ( nuke_directory -dir_to_nuke:$dir_to_remove -names_to_leave:$names_to_leave -leave_directory )
     return $null
 }
