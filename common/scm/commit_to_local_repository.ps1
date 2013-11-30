c3da7fc71c6d33d093d384e809fefdf844bc55bb
maass-sql <maassql@gmail.com>
2013-11-30 16:05:11 -0600

    wip

--- common/scm/commit_to_local_repository.ps1
+++ common/scm/commit_to_local_repository.ps1
@@ -6,12 +6,21 @@ Function commit_to_local_repository ($path_to_commit, $msg)
 
     write-host "commit_to_local_repository - Adding files" <# stage updates/deletes for ALL files, including new ones # Also a leading directory name (e.g. dir to add dir/file1 and dir/file2) can be given to add all files in the directory, recursively. #>
     $git_args = @('add', "--all" , "$path_to_commit")
-    $add_results = (& $git_path $git_args >> (log_file_name) 2>&1 )
+    try 
+    {
+        $add_results = (& $git_path $git_args >> (log_file_name) 2>&1 )
+    }
+    catch [Exception]
+    {
+        $throw = $true
+        $exception_Message = $nuke_exception.Message
+        if ($exception_Message -like "*warning: LF will be replaced by CRLF.*" )  { $throw = $false }
+        if ( $throw -eq $true ) { throw $exception_Message}
+    }    
     write-host "commit_to_local_repository of=[$path_to_commit] - added=[$add_results]"
 
 
     write-host "commit_to_local_repository - Committing files"<# stage updates/deletes for files git already knows about AND COMMIT #>
-
     $msg_arg = "--message='$($msg)'"
     $git_args = @('commit', $msg_arg)
     try 
