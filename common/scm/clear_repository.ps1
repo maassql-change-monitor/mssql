d19f41ed61b11e7c8422cd009fb12488971411bf
maass-sql <maassql@gmail.com>
2013-11-30 16:30:03 -0600

    wip

--- common/scm/clear_repository.ps1
+++ common/scm/clear_repository.ps1
@@ -4,7 +4,7 @@ function clear_repository($dir_to_remove)
     $names_to_leave = @('.git', '.gitignore')
     $null = ( nuke_directory -dir_to_nuke:$dir_to_remove -names_to_leave:$names_to_leave -leave_directory )
 
-    if ((Test-Path -LiteralPath:"$dir_to_remove\.git\index.lock") -eq $false )    
+    if ((Test-Path -LiteralPath:"$dir_to_remove\.git\index.lock") -eq $true )    
         { 
             Remove-Item -Force "$dir_to_remove\.git\index.lock" 
         }
