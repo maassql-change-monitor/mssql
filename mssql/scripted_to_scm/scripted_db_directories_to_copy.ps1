db01591f22b810db5ee95f5b24c7c39ea2dd2c7c
maass-sql <maassql@gmail.com>
2013-11-30 13:08:58 -0600

    allow user to throttle # of directories pulled at a time.  Will "help" allow multiple processes to work on the same directory.

--- mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
+++ mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
@@ -2,17 +2,17 @@ Function scripted_db_directories_to_copy ()
 {
     scripted_to_scm_log "scripted_db_directories_to_copy- BEGIN"
     $ret_array = @()
-    foreach ( $scripted_dir in Get-ChildItem -LiteralPath:$SCRIPT:scripted_db_directory_base_path ) <# -Directory is not in 2.0 #>
+    foreach ( $scripted_dir in Get-ChildItem -LiteralPath:$SCRIPT:scripted_db_directory_base_path `
+        | Where-Object { $_.PSIsContainer -eq $true } `
+        | Where-Object 
+            { 
+                $tspan=(New-TimeSpan $scripted_dir.LastAccessTime (Get-Date));
+                $diff_minutes=($tspan).minutes;
+                return ( $diff_minutes -ge $SCRIPT:scripted_db_directory_must_sit_idle_for_x_minutes ) 
+            }  `
+        | Select-Object $_ -First:$SCRIPT:directories_to_grab_at_a_time ) <# -Directory is not in 2.0 #>
     {
-        if ( $scripted_dir.PSIsContainer -eq $true)
-        {
-            $tspan=(New-TimeSpan $scripted_dir.LastAccessTime (Get-Date));
-            $diff_minutes=($tspan).minutes;
-            if ( $diff_minutes -ge $SCRIPT:scripted_db_directory_must_sit_idle_for_x_minutes )
-            {
-                $ret_array += $scripted_dir 
-            }
-        }
+        $ret_array += $scripted_dir 
     }
     
     scripted_to_scm_log "scripted_db_directories_to_copy -  DONE - Count of directories=[$($ret_array.Count)]"
