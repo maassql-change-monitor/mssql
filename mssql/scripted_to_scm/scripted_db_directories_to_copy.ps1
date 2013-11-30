0746f2b882d21dd9aefcf66c12f13074786b9975
maass-sql <maassql@gmail.com>
2013-11-30 13:44:25 -0600

    bug

--- mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
+++ mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
@@ -17,7 +17,7 @@ Function eligible_folders
     $ret_eligible = ( Get-ChildItem -LiteralPath:$SCRIPT:scripted_db_directory_base_path |
         Where-Object { $_.PSIsContainer -eq $true } |
         Where-Object { 
-                        $tspan=(New-TimeSpan $scripted_dir.LastAccessTime (Get-Date));
+                        $tspan=(New-TimeSpan $_.LastAccessTime (Get-Date));
                         $diff_minutes=($tspan).minutes;
                         return ( $diff_minutes -ge $SCRIPT:scripted_db_directory_must_sit_idle_for_x_minutes ) 
                      }  |
