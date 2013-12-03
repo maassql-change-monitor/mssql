beb4d8c193c8092ecca1b8ae754fee5c101a724c
maass-sql <maassql@gmail.com>
2013-12-03 15:41:34 -0600

    path too short

--- common/scm/create_repository.ps1
+++ common/scm/create_repository.ps1
@@ -36,7 +36,7 @@ Function create_repository
 
             commit_to_local_repository $full_path "first commit"  
         }
-    $repository_name > "$repository_path/.git/description"
+    $repository_name > "$full_path/.git/description"
 
 
 
