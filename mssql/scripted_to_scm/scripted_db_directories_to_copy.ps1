036d0477e4d89662f6c317a22bac03e6fb21996c
maass-sql <maassql@gmail.com>
2013-12-03 15:20:35 -0600

    app was bombing when there were no scripted dbs to work upon.

--- mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
+++ mssql/scripted_to_scm/scripted_db_directories_to_copy.ps1
@@ -21,7 +21,8 @@ Function eligible_folders
                         $diff_minutes=($tspan).minutes;
                         return ( $diff_minutes -ge $SCRIPT:scripted_db_directory_must_sit_idle_for_x_minutes ) 
                      }  |
-        Select-Object -Property:$_ -First:$SCRIPT:directories_to_grab_at_a_time
+        Select-Object -Property:$_ -First:$SCRIPT:directories_to_grab_at_a_time |
+        Where-Object { $_ -ne $null}
                     
         )
     return $ret_eligible
