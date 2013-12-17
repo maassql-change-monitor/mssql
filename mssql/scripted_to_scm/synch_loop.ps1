4b0b73ae0892cd37048beac80b4f87202ec325f2
maass-sql <maassql@gmail.com>
2013-12-17 14:12:51 -0600

    b

--- mssql/scripted_to_scm/synch_loop.ps1
+++ mssql/scripted_to_scm/synch_loop.ps1
@@ -2,7 +2,7 @@ function synch_loop ()
 {
     Set-StrictMode -Version:Latest
     $GLOBAL:ErrorActionPreference               = "Stop"
-    $sb = {scripted_to_scm}
+    $sb = {main_looped_function}
     $SCRIPT:dbs_put_into_scm = 0
     loop_your_code -hours_to_run:$SCRIPT:stop_the_script_after_X_hours -seconds_to_pause:$SCRIPT:synch_every_X_seconds -code_block_to_invoke:$sb
 }
