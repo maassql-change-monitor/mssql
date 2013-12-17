9faf305c466fb347a8cbfbc919f8a8949bd7d9f6
maass-sql <maassql@gmail.com>
2013-12-17 13:55:34 -0600

    s

--- common/common.ps1
+++ common/common.ps1
@@ -10,3 +10,4 @@ $my_dir             = ( Split-Path $my_fullname )
 . "$($my_dir )\loop_d_loop.ps1"
 Import-Module -Force "$($my_dir)\file_system_nuke\file_system_nuke.psm1" <# did you know that?  I did NOT know that!  ."path.psm1" --> actually opens the file.  In my case, in notepad #>
 . "$($my_dir )\scm\scm.ps1"
+. "$($my_dir )\logger\logger.psm1"
