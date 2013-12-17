f41ed049bb146cacfe49cc5504c7450cfb161db5
maass-sql <maassql@gmail.com>
2013-12-17 14:07:36 -0600

    b

--- common/common.ps1
+++ common/common.ps1
@@ -2,12 +2,12 @@ $error.clear();
 Set-StrictMode -Version:Latest
 $GLOBAL:ErrorActionPreference               = "Stop"
 
-$my_fullname        = ($MyInvocation.MyCommand.Definition)
-if ($my_fullname -eq $null -or $my_fullname -eq "" ) {throw "`$MyInvocation.MyCommand.Definition doesn't work the way I thought it does."}
-$PRIVATE:my_dir             = ( Split-Path $my_fullname )
+$common_ps1_fullname        = ($MyInvocation.MyCommand.Definition)
+if ($common_ps1_fullname -eq $null -or $common_ps1_fullname -eq "" ) {throw "`$MyInvocation.MyCommand.Definition doesn't work the way I thought it does."}
+$LOCAL:common_ps1_my_dir             = ( Split-Path $common_ps1_fullname )
 
 
-. "$($PRIVATE:my_dir )\loop_d_loop.ps1"
-Import-Module -Force "$($my_dir)\file_system_nuke\file_system_nuke.psm1" <# did you know that?  I did NOT know that!  ."path.psm1" --> actually opens the file.  In my case, in notepad #>
-. "$($PRIVATE:my_dir )\scm\scm.ps1"
-. "$($PRIVATE:my_dir )\logger\logger.psm1"
+. "$($LOCAL:common_ps1_my_dir )\loop_d_loop.ps1"
+Import-Module -Force "$($LOCAL:common_ps1_my_dir)\file_system_nuke\file_system_nuke.psm1" <# did you know that?  I did NOT know that!  ."path.psm1" --> actually opens the file.  In my case, in notepad #>
+. "$($LOCAL:common_ps1_my_dir )\scm\scm.ps1"
+. "$($LOCAL:common_ps1_my_dir )\logger\logger.psm1"
--- common/scm/scm.ps1
+++ common/scm/scm.ps1
@@ -2,18 +2,18 @@ $error.clear();
 Set-StrictMode -Version:Latest
 $GLOBAL:ErrorActionPreference               = "Stop"
 
-$my_fullname        = ($MyInvocation.MyCommand.Definition)
-if ($my_fullname -eq $null -or $my_fullname -eq "" ) {throw "`$MyInvocation.MyCommand.Definition doesn't work the way I thought it does."}
-$LOCAL:my_dir             = ( Split-Path $my_fullname )
+$scm_ps1_fullname        = ($MyInvocation.MyCommand.Definition)
+if ($scm_ps1_fullname -eq $null -or $scm_ps1_fullname -eq "" ) {throw "`$MyInvocation.MyCommand.Definition doesn't work the way I thought it does."}
+$LOCAL:scm_ps1_my_dir             = ( Split-Path $scm_ps1_fullname )
 
 Set-StrictMode -Version:Latest
 $GLOBAL:ErrorActionPreference               = "Stop"
 
-. "$($MyInvocation.MyCommand.Definition).vars.ps1"
-. "$($LOCAL:my_dir)\clear_repository.ps1"
-. "$($LOCAL:my_dir)\commit_to_local_repository.ps1"
-. "$($LOCAL:my_dir)\create_repository.ps1"
-. "$($LOCAL:my_dir)\git_exe.ps1"
-. "$($LOCAL:my_dir)\git_repo_exists.ps1"
-. "$($LOCAL:my_dir)\pack_repository.ps1"
-. "$($LOCAL:my_dir)\snapshot_commit.ps1"
+. "$($scm_ps1_fullname).vars.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\clear_repository.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\commit_to_local_repository.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\create_repository.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\git_exe.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\git_repo_exists.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\pack_repository.ps1"
+. "$($LOCAL:scm_ps1_my_dir)\snapshot_commit.ps1"
