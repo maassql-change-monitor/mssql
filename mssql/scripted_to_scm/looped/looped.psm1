12dccadc24e3232b982f25e6113165c18e160dca
maass-sql <maassql@gmail.com>
2013-12-17 14:17:39 -0600

    b

--- mssql/scripted_to_scm/looped/looped.psm1
+++ mssql/scripted_to_scm/looped/looped.psm1
@@ -5,7 +5,7 @@ $GLOBAL:ErrorActionPreference               = "Stop"
 $my_fullname        = ($MyInvocation.MyCommand.Definition)
 $my_dir             = ( Split-Path $my_fullname )
 
-code_common_directory=( Resolve-Path "$my_dir\..\..\..\common" )
+$code_common_directory=( Resolve-Path "$my_dir\..\..\..\common" )
 
 . "$($my_dir )\scripted_db_directories_to_copy.ps1"
 . "$($my_dir )\scripted_db_properties.ps1"
