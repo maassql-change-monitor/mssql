3d3b1f82301d8da437cc425b7a155e818b77992a
maass-sql <maassql@gmail.com>
2013-12-17 17:13:10 -0600

    b

--- common/scm/commit_to_local_repository.ps1
+++ common/scm/commit_to_local_repository.ps1
@@ -4,10 +4,20 @@ Function commit_to_local_repository ($path_to_commit, $msg)
 
     cd $path_to_commit
 
+    if ((Test-Path -LiteralPath:"$path_to_commit\.git\index.lock") -eq $true )  
+    {
+        throw ".git\index.lock exists.  NO BUENO!  path=[$path_to_commit\.git\index.lock]."
+    }
+
     write-host "commit_to_local_repository ADD BEGIN ------- of=[$path_to_commit]" <# stage updates/deletes for ALL files, including new ones # Also a leading directory name (e.g. dir to add dir/file1 and dir/file2) can be given to add all files in the directory, recursively. #>
     $git_args = @('add', "--all" , "$path_to_commit" )
     $null = ( git_exe -path_to_repository:$path_to_commit -da_args:$git_args  )
     write-host "commit_to_local_repository ADD DONE -------- of=[$path_to_commit]"
+
+    if ((Test-Path -LiteralPath:"$path_to_commit\.git\index.lock") -eq $true )  
+    {
+        throw "AFTER running the ADD, .git\index.lock exists.  NO BUENO!  path=[$path_to_commit\.git\index.lock]."
+    }
 
 
     write-host "commit_to_local_repository - Commiting - BEGIN | $path_to_commit"<# stage updates/deletes for files git already knows about AND COMMIT #>
@@ -15,6 +25,13 @@ Function commit_to_local_repository ($path_to_commit, $msg)
     $null = ( git_exe -path_to_repository:$path_to_commit -da_args:$git_args  )  
     <# TODO : look at $commit_results for [master ff61ceb] or for ? #>
     write-host "commit_to_local_repository - Commiting - DONE | $path_to_commit"    
+
+    if ((Test-Path -LiteralPath:"$path_to_commit\.git\index.lock") -eq $true )  
+    {
+        throw "AFTER running the COMMIT, .git\index.lock exists.  NO BUENO!  path=[$path_to_commit\.git\index.lock]."
+    }
+
+
     return $null
 }
 
