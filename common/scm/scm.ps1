ee47c559485c47eda25a2076fac2dd1597b7725c
maass-sql <maassql@gmail.com>
2013-12-17 14:04:04 -0600

    b

--- common/scm/scm.ps1
+++ common/scm/scm.ps1
@@ -4,16 +4,16 @@ $GLOBAL:ErrorActionPreference               = "Stop"
 
 $my_fullname        = ($MyInvocation.MyCommand.Definition)
 if ($my_fullname -eq $null -or $my_fullname -eq "" ) {throw "`$MyInvocation.MyCommand.Definition doesn't work the way I thought it does."}
-$PRIVATE:my_dir             = ( Split-Path $my_fullname )
+$LOCAL:my_dir             = ( Split-Path $my_fullname )
 
 Set-StrictMode -Version:Latest
 $GLOBAL:ErrorActionPreference               = "Stop"
 
 . "$($MyInvocation.MyCommand.Definition).vars.ps1"
-. "$($PRIVATE:my_dir)\clear_repository.ps1"
-. "$($PRIVATE:my_dir)\commit_to_local_repository.ps1"
-. "$($PRIVATE:my_dir)\create_repository.ps1"
-. "$($PRIVATE:my_dir)\git_exe.ps1"
-. "$($PRIVATE:my_dir)\git_repo_exists.ps1"
-. "$($PRIVATE:my_dir)\pack_repository.ps1"
-. "$($PRIVATE:my_dir)\snapshot_commit.ps1"
+. "$($LOCAL:my_dir)\clear_repository.ps1"
+. "$($LOCAL:my_dir)\commit_to_local_repository.ps1"
+. "$($LOCAL:my_dir)\create_repository.ps1"
+. "$($LOCAL:my_dir)\git_exe.ps1"
+. "$($LOCAL:my_dir)\git_repo_exists.ps1"
+. "$($LOCAL:my_dir)\pack_repository.ps1"
+. "$($LOCAL:my_dir)\snapshot_commit.ps1"
