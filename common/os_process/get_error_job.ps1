83e4df012ec477323348727c95295f8f0d246890
maass-sql <maassql@gmail.com>
2013-12-18 16:40:30 -0600

    b

--- common/os_process/get_error_job.ps1
+++ common/os_process/get_error_job.ps1
@@ -1,6 +1,6 @@
 Function get_error_job ( $process )
 {
-    write-host "setting up event receiver in get_output_job.  Is expected to log to:$( get_error_job )"
+    write-host "setting up event receiver in get_output_job.  Is expected to log to:$( log_file_name )"
 
     $errorjob = Register-ObjectEvent -InputObject $process -EventName ErrorDataReceived -SourceIdentifier Common.LaunchProcess.Error -action { event_receiver_error_data_received }
 
@@ -8,9 +8,7 @@ Function get_error_job ( $process )
         "ERROR - The error job is null" | Out-File ( log_file_name ) -Encoding ASCII -Append
         Write-Host "ERROR - The error job is null"
     }
-    else {
-        
-    }
+
 
     return $errorjob
 }
