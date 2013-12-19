7a20044800f48ae6543d16d6426e9304cd4ceb3f
maass-sql <maassql@gmail.com>
2013-12-19 07:38:52 -0600

    b

--- mssql/scripted_to_scm/main_looped_function.ps1
+++ mssql/scripted_to_scm/main_looped_function.ps1
@@ -64,6 +64,8 @@ function process_changes ( $changes, $commit_msg )
     write-host "----------------------------"
     write-host "$($changes | get-member | out-string)"
     write-host "----------------------------"
+    write-host "contains has_changes key=[$($changes.ContainsKey("has_changes"))]"
+    write-host "----------------------------"
     if ($changes."has_changes" -eq $true)
     {
         email_a_change $commit_msg $changes."filtered_output"
