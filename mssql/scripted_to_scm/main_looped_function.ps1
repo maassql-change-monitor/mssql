0454274d477614165617edd263b049cf32c8f438
maass-sql <maassql@gmail.com>
2013-12-19 15:11:35 -0600

    b

--- mssql/scripted_to_scm/main_looped_function.ps1
+++ mssql/scripted_to_scm/main_looped_function.ps1
@@ -136,8 +136,8 @@ function check_for_changes_html ( $scrptd , $change_detected )
     $who = "$($scrptd.'instance').$($scrptd.'dbname')"
     $checked = ($scrptd.'dttm')
     $url_diff = (url_last_change $scrptd)
-    $url_diff = "<a href='$url_diff'>Last Change</a>"
-    $html = "<table style='border:1px solid black;'><tr style='border:1px solid black;'><td>$checked</td><td>change=$change_detected</td><td>$who</td><td>$url_diff</td></tr></table>"
+    $url_diff = "<a href='$url_diff' target='_blank'>Last Change</a>"
+    $html = "<table style='border:1px solid black;'><tr style='border:1px solid black;'><td style='border:1px solid black;'>$checked</td><td style='border:1px solid black;'>change=$change_detected</td><td style='border:1px solid black;'>$who</td><td style='border:1px solid black;'>$url_diff</td></tr></table>"
     $html >> $SCRIPT:change_html_every
     if ( $change_detected -eq $true )
     {
