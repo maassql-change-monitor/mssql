05f8002a784d4f38726c6d1331e40f7b9043b3d8
maass-sql <maassql@gmail.com>
2013-12-18 23:12:44 -0600

    d

--- common/os_process/ProcessStdOut.ps1
+++ common/os_process/ProcessStdOut.ps1
@@ -4,6 +4,7 @@ Function Global:ProcessStd ( $stream, $string )
     [datetime]$dttm = (Get-Date)
     $dttm.ToUniversalTime().ToString("yyyyMMddzz HH:MM:SS")
     $new_line = "$([Environment]::NewLine)$dttm     |     $stream     |     $string"
+    write-output $new_line
     scripted_to_scm_log $new_line
     [string]$GLOBAL:stream = "$($GLOBAL:stream)$new_line"
     return $null
