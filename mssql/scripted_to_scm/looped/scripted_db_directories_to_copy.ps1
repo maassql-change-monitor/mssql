483c93857604c68e445ed3f9b0b8c79bff7d4d64
maass-sql <maassql@gmail.com>
2013-12-23 09:33:00 -0600

    getting it a bit closer to running multiple instances at once.  Set it up to ensure we are always processing the oldest directories first.

--- mssql/scripted_to_scm/looped/scripted_db_directories_to_copy.ps1
+++ mssql/scripted_to_scm/looped/scripted_db_directories_to_copy.ps1
@@ -1,12 +1,22 @@
 Function scripted_db_directories_to_copy ( $base_directory , $scripted_db_directory_must_sit_idle_for_x_minutes , $directories_to_grab_at_a_time)
 {
+
+    <#
+        Only look at directories
+        Only look at directories that have not been written to in X minutes
+        Sort those directories, oldest written to most recently written
+        Select the top X directories ( that have made it this far.... )
+        Where the objects are not null? ok...
+    #>
+
     $ret_eligible = ( Get-ChildItem -LiteralPath:$base_directory |
         Where-Object { $_.PSIsContainer -eq $true } |
         Where-Object { 
-                        $tspan=(New-TimeSpan $_.LastAccessTime (Get-Date));
+                        $tspan=(New-TimeSpan $_.LastWriteTimeUtc (Get-Date));
                         $diff_minutes=($tspan).minutes;
                         return ( $diff_minutes -ge $scripted_db_directory_must_sit_idle_for_x_minutes) 
                      }  |
+        Sort-Object $_.LastWriteTimeUtc | 
         Select-Object -Property:$_ -First:$directories_to_grab_at_a_time |
         Where-Object { $_ -ne $null}
                     
