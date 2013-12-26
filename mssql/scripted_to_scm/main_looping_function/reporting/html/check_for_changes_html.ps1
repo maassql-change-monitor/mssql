e879024f439845549477febfd7ee1fcad65139dd
maass-sql <maassql@gmail.com>
2013-12-26 14:06:51 -0600

    moving style to stylesheet

--- mssql/scripted_to_scm/main_looping_function/reporting/html/check_for_changes_html.ps1
+++ mssql/scripted_to_scm/main_looping_function/reporting/html/check_for_changes_html.ps1
@@ -4,7 +4,7 @@ function check_for_changes_html ( $scrptd , $change_detected )
     $checked = ($scrptd.'dttm')
     $url_diff = (url_last_change $scrptd)
     $url_diff = "<a href='$url_diff' target='_blank'>Last Change</a>"
-    $html = "<table style='border:1px solid black;'><tr style='border:1px solid black;'><td style='border:1px solid black;'>$checked</td><td style='border:1px solid black;'>change=$change_detected</td><td style='border:1px solid black;'>$who</td><td style='border:1px solid black;'>$url_diff</td></tr></table>"
+    $html = "<table class='db_change_check'><tr><td>$checked</td><td>change=$change_detected</td><td>$who</td><td>$url_diff</td></tr></table>"
     $html >> $SCRIPT:change_html_every
     if ( $change_detected -eq $true )
     {
