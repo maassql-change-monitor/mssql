cdb79f16368e85d1a5871b40d071060856f740b6
maass-sql <maassql@gmail.com>
2013-12-28 11:07:14 -0600

    grabbed some code changes from production, noted them here

--- mssql/scripted_to_scm/main_looping_function/reporting/infrastructure/html_file_names.ps1
+++ mssql/scripted_to_scm/main_looping_function/reporting/infrastructure/html_file_names.ps1
@@ -30,3 +30,33 @@ function stylesheet_file_name
 {
     return "$($my_dir)\html\scripted_to_scm_stylesheet.css" 
 }
+
+
+<#
+
+
+$this_day = (Get-Date).ToUniversalTime().ToString("yyyy.MM.dd")
+
+$SCRIPT:change_check_log = "$($my_dir)\html\checks_by_date_recorded\$($this_day) change_check_log.xml"
+$SCRIPT:change_html_every = "$($my_dir)\html\checks_by_date_recorded\$($this_day) every_check.html"
+$SCRIPT:change_html_changes = "$($my_dir)\html\checks_by_date_recorded\$($this_day) changes_detected.html"
+
+
+$SCRIPT:change_html_every_by_checked_date = "$($my_dir)\html\checks_by_date_checked\{dttm} every_check.html"
+$SCRIPT:change_html_changes_by_checked_date = "$($my_dir)\html\checks_by_date_checked\{dttm} changes_detected.html"
+
+
+
+    $st="style='border:1px solid black;'"
+    $html = "<table $($st)><tr $($st)><td $($st)>$checked</td><td>$now</td><td $($st)>$changed</td><td><a href='$url_diff' target='_blank'>$who</a></td></tr></table>"
+    
+    $html >> $SCRIPT:change_html_every
+    $html >> $SCRIPT:change_html_every_by_checked_date.Replace('{dttm}', $checked_as_date_string) 
+
+
+    if ( $change_detected -eq $true )
+    {
+      $html >> $SCRIPT:change_html_changes 
+      $html >> $SCRIPT:change_html_changes_by_checked_date.Replace('{dttm}', $checked_as_date_string) 
+    }
+#>
