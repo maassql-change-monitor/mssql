5ee2c17c51ada778f71f1097d0ae31367bf9a071
maass-sql <maassql@gmail.com>
2013-12-30 10:42:11 -0600

    b

--- mssql/scripted_to_scm/html_file_names.ps1
+++ mssql/scripted_to_scm/html_file_names.ps1
@@ -2,7 +2,8 @@ function html_file_report_every_check_by_date_checked ($dt_checked)
     {
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_by_date_checked\{dttm} changes_detected.html" 
         $html_file_name = ( replace_date ($dt_checked) $html_file_name )
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
+        write-host "returning filename=[$html_file_name]"
         return $html_file_name
     }
 
@@ -10,7 +11,7 @@ function html_file_report_changes_detected_by_date_checked ($dt_checked)
     {
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_by_date_checked\{dttm} every_check.html"
         $html_file_name = ( replace_date ($dt_checked) $html_file_name )
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name         
     }
 
@@ -18,7 +19,7 @@ function html_file_report_every_check_by_date_recorded
     {
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_by_date_recorded\{dttm} changes_detected.html"
         $html_file_name = ( replace_date (Get-Date) $html_file_name )
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name        
     }
 
@@ -26,21 +27,21 @@ function html_file_report_changes_detected_by_date_recorded
     {
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_by_date_recorded\{dttm} every_check.html"
         $html_file_name = ( replace_date (Get-Date) $html_file_name )
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name        
     }
 
 function html_file_report_every_check_by_instance($scrptd)
     {
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_per_instance\$($scrptd.instance) changes_detected.html"
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name        
     }
 
 function html_file_report_changes_detected_by_instance($scrptd)
     {
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_per_instance\$($scrptd.instance) every_check.html"
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name        
     }
 
@@ -49,7 +50,7 @@ function html_file_report_every_check_dbname($scrptd)
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_per_dbname\$($scrptd.dbname) {dttm} changes_detected.html"
         $dt_checked = ( scripted_checked_date $scrptd )
         $html_file_name = ( replace_date $dt_checked $html_file_name )
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name   
     }
 
@@ -58,7 +59,7 @@ function html_file_report_changes_detected_by_dbname($scrptd)
         $html_file_name = "$($SCRIPT:httpd_html_reports_loc)\checks_per_dbname\($scrptd.dbname) {dttm} every_check.html"
         $dt_checked = ( scripted_checked_date $scrptd )
         $html_file_name = ( replace_date $dt_checked $html_file_name )        
-        setup_html_file ($html_file_name)
+        $null = (setup_html_file ($html_file_name))
         return $html_file_name        
     }
 
