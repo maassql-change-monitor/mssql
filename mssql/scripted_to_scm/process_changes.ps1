acd75fb568c73e2a9fe5ca90ef3bef08d7dcbacc
maass-sql <maassql@gmail.com>
2013-12-31 11:12:57 -0600

    there are just too many files / reports

--- mssql/scripted_to_scm/process_changes.ps1
+++ mssql/scripted_to_scm/process_changes.ps1
@@ -42,7 +42,7 @@ function process_changes ( $changes, $scrptd )
         }
     else 
         {
-            write-host "We didn't detect any changes, so we are not going to alert anyone...has_changes=[$($change_detected)]"    
+            write-host "No changes detected."    
         } 
     return $null
 }
@@ -53,18 +53,18 @@ function create_reports_about_the_checkin ( $scrptd , $change_detected )
     $html = ( schema_checkin_as_html $scrptd  $change_detected )
     $checked_as_date = ( scripted_checked_date $scrptd )
 
-    
+    $null = ( shared_write_to_common_file ( html_file_report_every_check_by_date_recorded ) $html )
+    $null = ( shared_write_to_common_file ( html_file_report_every_check_by_date_checked ($checked_as_date) ) $html )
+    $null = ( shared_write_to_common_file ( html_file_report_every_check_by_instance $scrptd) $html )
+    $null = ( shared_write_to_common_file ( html_file_report_every_check_dbname $scrptd) $html )
 
-    $html >> ( html_file_report_every_check_by_date_recorded )
-    $html >> ( html_file_report_every_check_by_date_checked ($checked_as_date) )
-    $html >> ( html_file_report_every_check_by_instance $scrptd)
-    $html >> ( html_file_report_every_check_dbname $scrptd)
     if ( $change_detected -eq $true )
     {
-      $html >> ( html_file_report_changes_detected_by_date_recorded )
-      $html >> ( html_file_report_changes_detected_by_date_checked ($checked_as_date) )
-      $html >> ( html_file_report_changes_detected_by_instance $scrptd)
-      $html >> ( html_file_report_changes_detected_by_dbname $scrptd)
+
+        $null = ( shared_write_to_common_file ( html_file_report_changes_detected_by_date_recorded ) $html )
+        $null = ( shared_write_to_common_file  ( html_file_report_changes_detected_by_date_checked ($checked_as_date) ) $html )
+        $null = ( shared_write_to_common_file ( html_file_report_changes_detected_by_instance $scrptd) $html )
+        $null = ( shared_write_to_common_file ( html_file_report_changes_detected_by_dbname $scrptd) $html )
     }
     return $null
 }
@@ -80,9 +80,49 @@ function log_processing ( $scrptd , $change_detected )
     $scm_db_directory_full_path             = $scrptd.'scm_db_path'  
 
     $csv_line = "$pid , $(Get-Date) , $change_detected , $instance , $dbname , $dttm_scripted , $scripted_db_folder_name , $scm_name , $scripted_db_directory_full_path , $scm_db_directory_full_path" 
-    $csv_line >> ( processing_log_file )
+    $null = ( shared_write_to_common_file ( processing_log_file ) $csv_line )
     return $null
 }
 
+function shared_write_to_common_file ( $file_name, $to_write )
+{
+    $try_max = 5
+    do while ( (write_safe $file_name $to_write) -eq $false )
+    {
+        $try_max -= 1;
+        if ($try_max -le 0)
+        {
+            break
+        }
+        start-sleep -Seconds:1
+    }
+}
+
+function write_safe ( $file_name, $to_write )
+{
+    [boolean]$ret_written = $false
+
+    try 
+        {
+            $to_write >> ( $file_name )  
+            $ret_written = $true 
+        }
+    catch 
+        {
+            $to_log = "Exception whilst attempting to write to $file_name.  "
+            $ex = $null
+            if ($_.Exception -ne $null)
+            {
+                $ex = $_.Exception
+            }
+            if ( $ex -ne $null)
+            {
+                $to_log += $ex.Message
+            }
+            log_this $to_log
+        }
+
+    return $ret_written
+}
 
 
