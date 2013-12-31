2281d288d8cfd3299ec0d7f3944eec3874f24bf2
maass-sql <maassql@gmail.com>
2013-12-31 13:27:14 -0600

    b

--- mssql/scripted_to_scm/reports/url_check_history.ps1
+++ mssql/scripted_to_scm/reports/url_check_history.ps1
@@ -10,38 +10,54 @@ function url_reports_base
 =======================================================================================================================
 Check History
 #>
-function url_check_history_instance_every($instance)
-{
-    "$(url_reports_base)reports/"   
-}
-function url_check_history_instance_changes_only($instance)
-{
-    "$(url_reports_base)/reports/checks_by_instance/ASHSQL1_ASHPROD%20every_check.html"   
-}
-function url_check_history_database_every($db_name)
-{
-    "$(url_reports_base)/reports/checks_by_dbname/"   
-}
-function url_check_history_database_changes_only($db_name)
-{
-    "$(url_reports_base)/reports/checks_by_dbname/"   
-}
+
 function url_check_history_date_checked_every($date_scripted)
 {
-   "$(url_reports_base)/reports/checks_by_date_checked/{dttm}_every_check"
+   "$(url_reports_base)$(file_name_check_history_date_checked_every $date_scripted))" 
 }
 function url_check_history_date_checked_changes_only($date_scripted)
 {
-    "$(url_reports_base)/reports/checks_by_date_checked/{dttm}_every_check"
+    "$(url_reports_base)$(file_name_check_history_date_checked_changes_only $date_scripted))" 
 }
+
 function url_check_history_date_recorded_every($date_recorded)
 {
-    "$(url_reports_base)/reports/checks_by_date_recorded/2013.12.30%20changes_detected.html"
+    "$(url_reports_base)$(file_name_check_history_date_recorded_every $date_recorded))" 
 }
 function url_check_history_date_recorded_changes_only($date_recorded)
 {
-   "$(url_reports_base)/reports/checks_by_date_recorded/2013.12.30%20changes_detected.html" 
+   "$(url_reports_base)$(file_name_check_history_date_recorded_changes_only $date_recorded))" 
 }
+
+function url_check_history_instance_every($instance)
+{
+    "$(url_reports_base)$(file_name_check_history_instance_every $instance))"   
+}
+function url_check_history_instance_changes_only($instance)
+{
+    "$(url_reports_base)$(file_name_check_history_instance_changes_only $instance))"   
+}
+
+
+
+
+
+
+
+
+
+
+
+function url_check_history_database_every($db_name)
+{
+    "$(url_reports_base)$(file_name_check_history_database_every $db_name))"   
+}
+function url_check_history_database_changes_only($db_name)
+{
+    "$(url_reports_base)$(file_name_check_history_database_changes_only $db_name))"   
+}
+
+
 
 
 
