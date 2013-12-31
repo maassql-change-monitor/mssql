db4a31a85c16fb45ea1b1aa8bc43a7e2e76c3629
maass-sql <maassql@gmail.com>
2013-12-31 13:16:10 -0600

    b

--- mssql/scripted_to_scm/process_changes.ps1
+++ mssql/scripted_to_scm/process_changes.ps1
@@ -87,8 +87,7 @@ function log_processing ( $scrptd , $change_detected )
 function shared_write_to_common_file ( $file_name, $to_write )
 {
     $try_max = 5
-    do while ( (write_safe $file_name $to_write) -eq $false )
-    {
+    do while ( (write_safe $file_name $to_write) -eq $false ){
         $try_max -= 1;
         if ($try_max -le 0)
         {
