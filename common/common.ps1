ea7b10bac2cd631d207fde87ceb793126748d215
maass-sql <maassql@gmail.com>
2014-01-02 14:29:40 -0600

    b

--- common/common.ps1
+++ common/common.ps1
@@ -9,6 +9,6 @@ $LOCAL:common_ps1_my_dir             = ( Split-Path $common_ps1_fullname )
 . "$($LOCAL:common_ps1_my_dir )\file_system\file_system.ps1"
 Import-Module -Force "$($LOCAL:common_ps1_my_dir )\logger\logger.psm1"
 . "$($LOCAL:common_ps1_my_dir )\loop_d_loop\loop_d_loop.ps1"
-. "$($LOCAL:common_ps1_my_dir )\mssql\ExecNonQuery.psm1"
+Import-Module -Force "$($LOCAL:common_ps1_my_dir )\mssql\ExecNonQuery.psm1"
 . "$($LOCAL:common_ps1_my_dir )\os_process\os_process.ps1"
 . "$($LOCAL:common_ps1_my_dir )\scm\scm.ps1"
