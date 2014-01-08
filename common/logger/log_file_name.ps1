32f8792962e587178245513f3f7d736fda8f7194
maass-sql <maassql@gmail.com>
2014-01-08 11:55:19 -0600

    try to be more organized with logs

--- common/logger/log_file_name.ps1
+++ common/logger/log_file_name.ps1
@@ -1,4 +1,6 @@
 Function GLOBAL:log_file_name
 {
-    return "$($SCRIPT:Log_directory)\$($GLOBAL:log_me_as_logger_name)_$(get_sortable_date_hour)_pid.$pid.log"
+    $log_now_folder="$($SCRIPT:Log_directory)\$(get_sortable_date_hour)\"
+    New-Item $log_now_folder -type directory -force
+    return "$log_now_folder\$($GLOBAL:log_me_as_logger_name)_$(get_sortable_date_hour)_pid.$pid.log"
 }
