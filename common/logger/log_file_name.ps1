4fef920a9042710a858a7562e6d5b7cb7f2b0fb4
maass-sql <maassql@gmail.com>
2014-01-08 12:01:47 -0600

    return only the path, nothing else

--- common/logger/log_file_name.ps1
+++ common/logger/log_file_name.ps1
@@ -1,6 +1,6 @@
 Function GLOBAL:log_file_name
 {
     $log_now_folder="$($SCRIPT:Log_directory)\$(get_sortable_date_hour)\"
-    New-Item $log_now_folder -type directory -force
+    $null = (New-Item -path:$log_now_folder -type:directory -force)
     return "$log_now_folder\$($GLOBAL:log_me_as_logger_name)_$(get_sortable_date_hour)_pid.$pid.log"
 }
