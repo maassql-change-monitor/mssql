813afe241f1a3787e105cd23cbc53400e0806c32
maass-sql <maassql@gmail.com>
2014-01-09 13:54:07 -0600

    share the log files, now that we have jobs running, writing to the same file.

--- common/logger/log_this.ps1
+++ common/logger/log_this.ps1
@@ -1,5 +1,5 @@
 Function GLOBAL:log_this ($to_log, $log_name=$null)
 {
     $to_log = "$(Get-Date) | $to_log" 
-    $to_log >> (log_file_name $log_name)
+    $null = ( shared_write_to_common_file (log_file_name $log_name) $to_log )
 }
