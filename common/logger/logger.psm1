261b409e1091365a98a9b8bc8cf6f0087fff430a
maass-sql <maassql@gmail.com>
2014-01-09 13:56:12 -0600

    b

--- common/logger/logger.psm1
+++ common/logger/logger.psm1
@@ -10,5 +10,6 @@ $SCRIPT:Log_directory = $my_dir
 . "$($my_dir )\log_file_name.ps1"
 . "$($my_dir )\log_me_as.ps1"
 . "$($my_dir )\log_this.ps1"
+. "$($my_dir )\..\file_system\shared_write_to_common_file.ps1"
 
 $SCRIPT:log_me_as_logger_name = 'log_this'
